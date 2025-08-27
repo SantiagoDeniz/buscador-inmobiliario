939cd7afc904e604e2f8bc8ce8da7f2ef03ddbd0
Santiago Deniz <Santiago.Deniz.ti@gmail.com>
2025-08-26 22:37:30 -0300

    recuando. No funcionó

--- core/scraper.py
+++ core/scraper.py
@@ -3,7 +3,7 @@ def iniciar_driver():
     chrome_options = Options()
     
     # Configuraciones básicas para headless
-#    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
